fcec45224a7684abc2c1e835d0518c8515058d19
matejmusap <matej.musap@gmail.com>
2018-11-06 10:38:59 +0100

    6-11-2018

--- yelp.js
+++ yelp.js
@@ -1,47 +1,48 @@
-var express        = require("express"),
-    app            = express(),
-    dotenv         = require('dotenv').config(),
-    bodyParser     = require("body-parser"),
-    mongoose       = require("mongoose"),
-    flash          = require("connect-flash"),
-    passport       = require("passport"),
-    LocalStrategy  = require("passport-local"),
-    methodOverride = require("method-override"),
-    Campground     = require("./models/campground"),
-    Comment        = require("./models/comment"),
-    User           = require("./models/user"),
-    
-//requiring routes
-    commentRoutes    = require("./routes/comments"),
-    campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes      = require("./routes/index"),
-    contactRoutes    = require("./routes/contact");
-    
+var express = require("express"),
+  app = express(),
+  dotenv = require("dotenv").config(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  Campground = require("./models/campground"),
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  //requiring routes
+  commentRoutes = require("./routes/comments"),
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index"),
+  contactRoutes = require("./routes/contact");
+
 mongoose.connect("mongodb://localhost/yelp_camp");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-app.locals.moment = require('moment');
+app.locals.moment = require("moment");
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
-    secret: "Ljiljana is the most beatiful girl in the world!",
+app.use(
+  require("express-session")({
+    secret: process.env.SECRET_WORDS,
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   res.locals.error = req.flash("error");
-   res.locals.success = req.flash("success");
-   next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use("/", indexRoutes);
@@ -49,7 +50,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/contact", contactRoutes);
 
-
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log("The YelpCamp Server Has Started!");
 });
